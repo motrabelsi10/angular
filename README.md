5a57e4486043556a78e2ddc2507bec9c28e6bf33
motrabelsi10 <trabelsi.mohamed.1@esprit.tn>
2024-03-24 17:30:38 +0100

    squelette

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Angular
+# BackTest
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.10.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.0.
 
 ## Development server
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EssaiComponent } from './essai/essai.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [{ path: 'essai', component: EssaiComponent },
+                       ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
