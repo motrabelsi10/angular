91790033f91f76df4d5e30b98dcf9359167af1da
amanibensassi <123634459+amanibensassi@users.noreply.github.com>
2024-03-28 16:54:49 +0100

    enumeration

--- src/app/models/equipement.ts
+++ src/app/models/equipement.ts
@@ -1,17 +1,20 @@
+import { Equip } from "./equip";
+import { Typeequip } from "./typeequip";
+
 export class Equipement {
     idEquipement : number;
-    equipement  : string;
+    equipement  : Equip;
     other  : string;
-    TypeEquip : string;
+    TypeEquip : Typeequip;
     approuvement : boolean;
     price : number;
 
 
     constructor(){
         this.idEquipement = 0;
-        this.equipement = "";
+        this.equipement = Equip.other;
         this.other = "";
-        this.TypeEquip = "";
+        this.TypeEquip = Typeequip.nothing,
         this.approuvement = false;
         this.price = 0;
       }
--- src/app/models/management.ts
+++ src/app/models/management.ts
@@ -1,2 +1,23 @@
+import { Bloc } from "./bloc";
+import { Classe } from "./classe";
+
 export class Management {
+    idManagement : number;
+    bloc  : Bloc;
+    classe : Classe;
+    heureStart : Date;
+    heureFinish : Date;
+    approuvement : boolean;
+    event : number;
+
+
+    constructor(){
+        this.idManagement = 0;
+        this.bloc = Bloc.A;
+        this.classe = Classe.cours;
+        this.heureStart = new Date();
+        this.heureFinish = new Date();
+        this.approuvement = false;
+        this.event = 0;
+      }
 }
