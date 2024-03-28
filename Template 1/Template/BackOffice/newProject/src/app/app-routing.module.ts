afc8562c3ba11130e96d6bfe8dd4710e37b0b33a
amanibensassi <123634459+amanibensassi@users.noreply.github.com>
2024-03-28 11:10:30 +0100

    2nd template

---
+++ Template 1/Template/BackOffice/newProject/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EssaiComponent } from './essai/essai.component';
+
+const routes: Routes = [{ path: 'essai', component: EssaiComponent },];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
